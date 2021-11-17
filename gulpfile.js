4548875aa8ac3c77fe4f0b3fc27966cbf940259c
CANDIDO, Lucas <lcand2057@gmail.com>
2021-11-18 02:59:12 +1100

    setup gulp sass comilation

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const sourcemaps = require('gulp-sourcemaps'),
+  autoprefixer = require('gulp-autoprefixer'),
+  sass = require('gulp-sass')(require('sass')),
+  gulp = require('gulp');
+
+function build() {
+  return gulp.src('./_sass/*.sass')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./style'));
+}
+
+exports.default = build;
+exports.watch()
